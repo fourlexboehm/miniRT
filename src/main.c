0fe04fa9418b7c253a4aebb883ee54a46e121cb3
Alex Boehm <alexboehm@systemli.org>
2022-09-19 13:39:35 +0930

    Fix arg handling

--- src/main.c
+++ src/main.c
@@ -6,16 +6,11 @@ int	main(int argc, char **argv)
 	t_scene		scene;
 	t_vector3	**v_matrix;
 	int			**c_matrix;
-	char 		*file;
 
 	if (argc == 2)
-	{
-		file = ft_strjoin("scenes/", argv[1]);
-		line = get_file(file);
-		free(file);
-	}
+		line = get_file(argv[1]);
 	else
-		line = get_file("scenes/subject_example2.rt");
+		line = get_file("scenes/subject_example3.rt");
 	assign_scene(&scene, line);
 	v_matrix = calculate_viewport_vectors(scene.camera);
 	c_matrix = set_colour_matrix(v_matrix, scene);
